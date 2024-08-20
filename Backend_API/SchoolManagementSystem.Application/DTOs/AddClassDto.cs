d004e9619848bc608d630c95739cf59b8f45f536
shari1434 <shari@wavestechnology.net>
2024-08-20 15:45:14 +0500

    added dto folder with class dto

---
+++ Backend_API/SchoolManagementSystem.Application/DTOs/AddClassDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolManagementSystem.Application.DTOs
+{
+    public class AddClassDto
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public int Capacity { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public bool IsActive { get; set; } = true;
+
+
+
+    }
+}
