87056ed29d57de32542041727fcf206b90e3267f
shari1434 <shari@wavestechnology.net>
2024-09-03 20:07:28 +0500

    applicant work added

--- Backend_API/SchoolManagementSystem.Application/Mappers/ApplicantMapper.cs
+++ Backend_API/SchoolManagementSystem.Application/Mappers/ApplicantMapper.cs
@@ -1,10 +1,5 @@
 ﻿using SchoolManagementSystem.Application.DTOs;
 using SchoolManagementSystem.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SchoolManagementSystem.Application.Mappers
 {
