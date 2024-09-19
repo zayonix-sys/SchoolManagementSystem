8ff163a9484a510e54e467bdf15c19e168124e4a
shari1434 <shari@wavestechnology.net>
2024-09-19 19:42:05 +0500

    user-profile addeed

---
+++ Frontend_UI/app/[lang]/(dashboard)/user-profile/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserInfo from './sponsor-profile'
+import UserMeta from './user-meta'
+import Header from './components/header'
+// import UserMeta from '@/components/user-profile/settings/user-meta'
+
+const  UserProfile = () => {
+  return (
+    <>
+   <Header/>
+          <UserMeta/>
+
+      
+    </>
+  )
+}
+
+export default UserProfile
