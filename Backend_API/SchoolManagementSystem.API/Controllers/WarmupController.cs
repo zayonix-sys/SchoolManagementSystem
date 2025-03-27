4dba2e1af3b2a361df76449a5682480f1a8fd8c4
Anwar Ul Islam <annu.anwar2000@gmail.com>
2025-03-27 10:31:57 +0500

    warmup controller

---
+++ Backend_API/SchoolManagementSystem.API/Controllers/WarmupController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarmupController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Web API is up and running.");
+        }
+    }
+}
